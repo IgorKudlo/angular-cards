2b6f08e9c2a03e0ca87e5df96e1de5ab8938cb41
igor.kudlo <igoreklida@gmail.com>
2024-01-29 18:16:32 +0300

    Sign In, Sign Up

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
@@ -16,8 +18,20 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
+    RouterLink,
+    NgIf,
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
-export class AuthComponent {}
+export class AuthComponent implements OnInit {
+  authType = '';
+  title = '';
+
+  constructor(private readonly route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.authType = this.route.snapshot.url.at(-1)!.path;
+    this.title = this.authType === 'login' ? 'Sign In' : 'Sign Up';
+  }
+}
