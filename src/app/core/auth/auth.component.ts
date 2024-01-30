bd1a238400c917a594245eeef03b3cb57b86e40c
igor.kudlo <igoreklida@gmail.com>
2024-01-30 11:04:23 +0300

    auth form fields

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -3,10 +3,23 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { NgIf } from '@angular/common';
+
+interface AuthForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
+  confirmPassword?: FormControl<string>;
+  rememberMe?: FormControl<boolean>;
+}
 
 @Component({
   selector: 'app-auth',
@@ -20,6 +33,7 @@ import { NgIf } from '@angular/common';
     MatCheckboxModule,
     RouterLink,
     NgIf,
+    ReactiveFormsModule,
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
@@ -27,11 +41,44 @@ import { NgIf } from '@angular/common';
 export class AuthComponent implements OnInit {
   authType = '';
   title = '';
+  authForm: FormGroup<AuthForm>;
 
-  constructor(private readonly route: ActivatedRoute) {}
+  constructor(private readonly route: ActivatedRoute) {
+    this.authForm = new FormGroup<AuthForm>({
+      email: new FormControl('', {
+        validators: [Validators.required],
+        nonNullable: true,
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required],
+        nonNullable: true,
+      }),
+    });
+  }
 
   ngOnInit(): void {
     this.authType = this.route.snapshot.url.at(-1)!.path;
     this.title = this.authType === 'login' ? 'Sign In' : 'Sign Up';
+    if (this.authType === 'login') {
+      this.authForm.addControl(
+        'rememberMe',
+        new FormControl(false, {
+          nonNullable: true,
+        }),
+      );
+    }
+    if (this.authType === 'register') {
+      this.authForm.addControl(
+        'confirmPassword',
+        new FormControl('', {
+          validators: [Validators.required],
+          nonNullable: true,
+        }),
+      );
+    }
+  }
+
+  submitAuthForm() {
+    console.log(this.authForm);
   }
 }
